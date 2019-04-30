c751ebe3ac15776590117a6aa8c6d6a84672f58c
chanminjun <justinjun900@gmail.com>
2019-04-30 17:15:06 +0900

    index crawler added

---
+++ fin_ml.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+
+def get_weights(d, size):
+    w = [1.]
+    for k in range(1, size):
+        w_=-w[-1]/k*(d-k+1)
+        w.append(w_)
+    w = np.array(w[::-1]).reshape(-1, 1)
+    return w
+
+
+def plot_weigths(d_range, n_plots, size):
+    w = pd.DataFrame()
+    for d in np.linspace(d_range[0], d_range[1], n_plots):
+        w_ = get_weights(d, size=size)
+        w_ = pd.DataFrame(w_, index=range(w_.shape[0])[::-1], columns=[d])
+        w = w.join(w_, how="outer")
+    ax = w.plot()
+    ax.legend(loc="upper left")
+    mpl.show()
+    return
+
+
+def frac_diff(series, d, thres=.01):
+    w = get_weights(d, series.shape[0])
+    w_ = np.cumsum(abs(w))
+    w_ /= w_[-1]
+    skip=w_[w_ > thres].shape[0]
+    df = {}
+    for name in series.columns:
+        series_f, df_ = series[[name]].fillna(method="ffill").dropna(), pd.Series()
+        for iloc in range(skip, series_f.shape[0]):
+            loc = series_f.index[iloc]
+            if not np.isfinite(series.loc[loc, name]): continue
+            df[loc] = np.dot(w[-(iloc + 1):, :].T, series_f.loc[:loc])[0, 0]
+        df[name] = df_.copy(deep=True)
+    df = pd.concat(df, axis=1)
+    return df
+
+
+def get_weights_ffd(d, thres):
+    w, k = [1.], 1
+    while True:
+        w_ = -w[-1] / k * (d-k+1)
+        if abs(w_) < thres: 
+            break
+        w.append(w_)
+        k += 1
+    w = np.array(w[::-1]).reshape(-1, 1)
+    plt.plot(w)
+    plt.show()
+    return w
+
+
+def frac_dff_ffd(series, d, thres=1e-4):
+    w = get_weights_ffd(d, thres)
+    width = len(w) - 1
+    df = {}
+    for name in series.columns:
+        series_f = series[[name]].fillna(method="ffill").dropna()
+        df_ = pd.Series()
+        for iloc1 in range(width, series_f.shape[0]):
+            loc0, loc1 = series_f.index[iloc1 - width], series_f.index[iloc1]
+            if not np.isfinite(series.loc[loc1, name]): continue
+            df_[loc1] = np.dot(w.T, series_f.loc[loc0: loc1])[0, 0]
+        df[name] = df_.copy(deep=True)
+    df = pd.concat(df, axis=1)
+    return df
+
+
---
+++ krx_index.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def to_float(element):
+    if (type(element) == str) and (len(element) != 0):
+        res = element.replace('\"', "")
+        return float(res)
+    
+class krx_indus_index():
+    
+    base_url = "http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx?name=fileDown&filetype=csv&url=MKD/13/1301/13010102/mkd13010102&type=2&ind_type={}&period_strt_dd={}&period_end_dd={}&pagePath=%2Fcontents%2FMKD%2F13%2F1301%2F13010102%2FMKD13010102.jsp"
+    df_columns = ["date", "close", "chg", "%chg", "open", "high", "low", "vol", "vol_won", "mkt_cap"]
+    
+    codes_list = pd.read_csv("./krx_core/krx_index_indexno_csv.csv", header="infer")
+    
+    def __init__(self):
+        pass
+    
+    def get_df_by_code(self, start="19000101", end="30000101", index_no="5300"):
+        self.url = self.base_url.format(index_no, start, end)
+        response = requests.get(self.url)
+        download_url = "http://file.krx.co.kr/download.jspx"
+        json_data = {"code": response.content}
+        headers_json = {"Referer": "http://marketdata.krx.co.kr/contents/MKD/99/MKD9900001.jspx"}
+        data = requests.post(download_url, data=json_data, headers=headers_json)
+        parsing = BeautifulSoup(data.text)
+        parsing = parsing.text.split("\n")
+        parsing = [line.replace(",", "").strip('"') for line in parsing]
+        parsing = [x.split('""') for x in parsing]
+        parsing = pd.DataFrame(parsing[1:], columns=self.df_columns)
+        parsing.index = pd.to_datetime(parsing["date"], format="%Y/%m/%d")
+        parsing.drop("date", axis=1, inplace=True)
+        self.data = parsing.astype("float64")
+        return self.data
+        
