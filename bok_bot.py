bd1e5b7b50623761456800a020e78b052a4e4cbf
chanminjun <justinjun900@gmail.com>
2019-05-02 14:15:13 +0900

    further file removal

--- bok_bot.py
+++ bok_bot.py
@@ -4,11 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rc
 
-rc("font", family="AppleGothic")
-plt.rcParams["axes.unicode_minus"] = False
+class bok_api:
 
-class bok_api():
-    
     key = "RK4O8KBR4N0O4E2W6V9E"
     url_base = "http://ecos.bok.or.kr/api/"
     table = None
@@ -20,17 +17,17 @@ class bok_api():
     }
     
     def __init__(self):
-        self.url = self.url_base + "StatisticTableList/{}/json/kr/0/{}".format(self.key, str(1000))
-        result = requests.get(self.url)
+        url = self.url_base + "StatisticTableList/{}/json/kr/0/{}".format(self.key, str(1000))
+        result = requests.get(url)
         result = pd.DataFrame(result.json()["StatisticTableList"]["row"])
         self.table = result
     
     def get_stat_list_detail(self, rows=1000, stat_code=""):
         if stat_code == "":
             raise ValueError("Stat code not provided! Please insert stat code!")
-        self.url = self.url_base + "StatisticItemList/{}/json/kr/0/{}/{}".format(self.key, str(rows), stat_code)
+        url = self.url_base + "StatisticItemList/{}/json/kr/0/{}/{}".format(self.key, str(rows), stat_code)
         self.stat_code = stat_code
-        result = requests.get(self.url)
+        result = requests.get(url)
         result = pd.DataFrame(result.json()["StatisticItemList"]["row"])
         self.table_detail = result
     
@@ -42,12 +39,11 @@ class bok_api():
         self.item_code = item_code
         self.period = self.table_detail[self.table_detail["ITEM_CODE"] == self.item_code]["CYCLE"].values[0]
         data_format = self.data_format[self.period]
-        self.url = self.url_base + "StatisticSearch/{}/json/kr/0/{}/{}/{}/{}/{}/{}".format(self.key, rows, self.stat_code, self.period, start, end, self.item_code)
-        result = requests.get(self.url)
+        url = self.url_base + "StatisticSearch/{}/json/kr/0/{}/{}/{}/{}/{}/{}".format(self.key, rows, self.stat_code, self.period, start, end, self.item_code)
+        result = requests.get(url)
         result = pd.DataFrame(result.json()["StatisticSearch"]["row"])
-        self.data = result
-        self.data = self.data[["TIME", "DATA_VALUE"]]
+        self.data = result[["TIME", "DATA_VALUE"]]
         self.data.index = pd.to_datetime(self.data["TIME"], format=data_format)
         self.data = self.data["DATA_VALUE"].replace("", np.nan)
         return self.data
-    
+
