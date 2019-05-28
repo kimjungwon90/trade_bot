43f47b4fdc1a9077f3634d41231aefbc25f57423
chanminjun <justinjun900@gmail.com>
2019-05-28 19:24:27 +0900

    techinical experiment

--- cm_jun_res_ch_4.py
+++ cm_jun_res_ch_4.py
@@ -1,29 +1,59 @@
 #%%
-import naver_crawler
 import matplotlib.pyplot as plt
-import technicals
-import sklearn
+import pandas as pd
+import numpy as np
 import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+import naver_crawler
+import technicals
 
 sns.set_style("white")
 
 test_api = naver_crawler.naver_stock_crawler()
-test_data = test_api.get_stock("069500").loc["2016-01-01":, :]
+test_data = test_api.get_stock("069500").loc["2010-06-01":, :]
 
-#%%
 ichi_moku = technicals.ichimoku(test_data["hv"], test_data["lv"], test_data["ncv"])
 
-#%%
+df_test = pd.DataFrame()
+df_test["close"] = test_data["ncv"]
+df_test["kenkansen"] = ichi_moku[0] / df_test["close"]
+df_test["kijunsen"] = ichi_moku[1] / df_test["close"]
+df_test["leading_a"] = ichi_moku[2] / df_test["close"]
+df_test["leading_b"] = ichi_moku[3] / df_test["close"]
+df_test["cloud"] = (ichi_moku[2] > ichi_moku[3]).astype("int8")
+# df_test["chikou"] = ichi_moku[4] / df_test["close"]
+df_test["return"] = (df_test["close"] / df_test["close"].shift(1)).apply(np.log)
+df_test["class"] = (df_test["return"] > 0).astype("int8").shift(-1)
+df_test["close"] = df_test["close"] / df_test["close"]
+
+df_test.dropna(inplace=True)
+
+df_X = df_test.drop(["return", "class"], axis=1)
+df_y = df_test["class"]
+
+tree = RandomForestClassifier(max_depth=5, n_estimators=100)
+tree.fit(df_X.loc[:"2016-12-31"], df_y[:"2016-12-31"])
+
+y_hat = tree.predict_proba(df_X)
+df_test["pos"] = tree.predict(df_X)
+df_test.loc["2016-12-31":, "trade_return"] = df_test["pos"].shift() * df_test["return"].shift(-2)
+df_test["trade_return"].cumsum().plot()
+
+
+
+
 plt.figure(figsize=(12, 8))
-plt.plot(test_data["ncv"], label="Close")
-plt.plot(ichi_moku[0].shift(-26), ls="--", label="kenkansen")
-plt.plot(ichi_moku[1].shift(-26), ls="--", label="kijunsen")
-plt.plot(ichi_moku[2].shift(26), ls="--", label="leading_a")
-plt.plot(ichi_moku[3].shift(26), ls="--", label="leading_b")
+plt.plot(test_data["ncv"], label="Close", color="black")
+plt.plot(ichi_moku[0], ls="--", label="kenkansen", color="blue")
+plt.plot(ichi_moku[1], ls="--", label="kijunsen")
 plt.fill_between(test_data["ncv"].index, ichi_moku[2], ichi_moku[3], ichi_moku[2] > ichi_moku[3], color="green", alpha=0.333)
 plt.fill_between(test_data["ncv"].index, ichi_moku[2], ichi_moku[3], ichi_moku[2] < ichi_moku[3], color="red", alpha=0.333)
-plt.plot(ichi_moku[4].shift(-26), ls="--", label="chikou")
+plt.plot(ichi_moku[4], ls="--", label="chikou", color="purple")
 plt.legend()
 plt.show()
 
+#%%
+df_test.tail()
 #%%
--- technicals.py
+++ technicals.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 
 def ichimoku(highs, lows, close):
-    kenkansen = (highs.rolling(9).max() + lows.rolling(9).min()) / 2
-    kijunsen = (highs.rolling(26).max() + lows.rolling(26).min()) / 2
-    leading_span_a = (kenkansen + kijunsen) / 2
-    leading_span_b = (highs.rolling(52).max() + lows.rolling(52).min()) / 2
-    lagging_span = close.shift(26)
+    kenkansen = ((highs.rolling(9).max() + lows.rolling(9).min()) / 2)
+    kijunsen = ((highs.rolling(26).max() + lows.rolling(26).min()) / 2)
+    leading_span_a = ((kenkansen + kijunsen) / 2).shift(26)
+    leading_span_b = ((highs.rolling(52).max() + lows.rolling(52).min()) / 2).shift(26)
+    lagging_span = close.shift(-26)
     return kenkansen, kijunsen, leading_span_a, leading_span_b, lagging_span
+
+
+def money_flow_index(high, low, close, volume):
+    typical_price = (high + low + close) / 3
+    money_flow = typical_price * volume
+    pos_mf = money_flow
 
